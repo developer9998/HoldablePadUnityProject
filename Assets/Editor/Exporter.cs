ddb477f3548cd4913bae59b3e7bf522f4fa24050
dev9998 <81720436+developer9998@users.noreply.github.com>
2022-06-28 21:10:26 -0230

    exporter update

--- Assets/Editor/Exporter.cs
+++ Assets/Editor/Exporter.cs
@@ -1,128 +1,44 @@
-﻿using UnityEngine;
-using UnityEditor;
-using System.IO;
-using UnityEngine.UI;
-
-public class Exporter
-{
-    [MenuItem("Holdable Pad/Build Holdable")]
-    static void BuildAllAssetBundles()
-    {
-
-        if (!Selection.activeGameObject)
-        {
-            EditorUtility.DisplayDialog("Build Failed", "Please select the GameObject you want to build.", "OK");
-            return;
-        }
-
-        var selectedObject = Selection.activeGameObject;
-
-        if (!selectedObject.GetComponent<Descriptor>())
-        {
-            EditorUtility.DisplayDialog("Build Failed", "Please add the Descriptor component to your empty GameObject.", "OK");
-            return;
-        }
-
-        Descriptor descriptor = selectedObject.GetComponent<Descriptor>();
-        if (!AssetDatabase.IsValidFolder("Assets/ExportedHoldables"))
-        {
-            AssetDatabase.CreateFolder("Assets", "ExportedHoldables");
-        }
-
-        string holdableName = descriptor.Name;
-        string holdableAuthor = descriptor.Author;
-        string holdableDescription = descriptor.Description;
-        string holdableLHand = descriptor.leftHand.ToString();
-        string empty = "";
-        bool includedName = holdableName != empty;
-        bool includedAuthor = holdableAuthor != empty;
-        bool includedDescription = holdableDescription != empty;
-
-        if (!includedName)
-        {
-            EditorUtility.DisplayDialog("Build Failed", "Please include a name in your Descriptor component.", "OK");
-            return;
-        }
-        else if (!includedAuthor)
-        {
-            EditorUtility.DisplayDialog("Build Failed", "Please include an author in your Descriptor component.", "OK");
-            return;
-        }
-        else if (!includedDescription)
-        {
-            EditorUtility.DisplayDialog("Build Failed", "Please include a description in your Descriptor component.", "OK");
-            return;
-        }
-
-        string prefabPath = "Assets/ExportedHoldables/holdABLE.prefab";
-
-        AssetDatabase.SaveAssets();
-        AssetDatabase.Refresh();
-        PrefabUtility.SaveAsPrefabAsset(selectedObject.gameObject, prefabPath);
-        GameObject contentsRoot = PrefabUtility.LoadPrefabContents(prefabPath);
-        contentsRoot.name = "holdABLE";
-
-        Text player_info = contentsRoot.AddComponent<Text>();
-        string split = "$"; // splits each of the strings
-        player_info.text = holdableName + split + holdableAuthor + split + holdableDescription + split + holdableLHand;
-
-        Object.DestroyImmediate(contentsRoot.GetComponent<Descriptor>());
-        if (File.Exists(prefabPath))
-        {
-            File.Delete(prefabPath);
-        }
-
-        string newprefabPath = "Assets/ExportedHoldables/" + contentsRoot.name + ".prefab";
-        PrefabUtility.SaveAsPrefabAsset(contentsRoot, newprefabPath);
-        PrefabUtility.UnloadPrefabContents(contentsRoot);
-        AssetImporter.GetAtPath(newprefabPath).SetAssetBundleNameAndVariant("holdableAssetBundle", "");
-
-        string assetBundleDirectory = "Assets/ExportedHoldables";
-        if (!Directory.Exists("Assets/ExportedHoldables"))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-
-        string asset_new = assetBundleDirectory + "/" + holdableName;
-        if (File.Exists(asset_new + ".holdable"))
-        {
-            File.Delete(asset_new + ".holdable");
-        }
-
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
-        if (File.Exists(newprefabPath))
-        {
-            File.Delete(newprefabPath);
-        }
-
-        string asset_manifest = assetBundleDirectory + "/holdableAssetBundle.manifest";
-        Debug.Log(asset_manifest);
-        if (File.Exists(asset_manifest))
-        {
-            File.Delete(asset_manifest);
-        }
-
-        string folder_manifest = assetBundleDirectory;
-        if (File.Exists(folder_manifest))
-        {
-            File.Delete(folder_manifest);
-
-            File.Delete(folder_manifest + ".manifest");
-        }
-
-        string asset_temporary = assetBundleDirectory + "/holdableAssetBundle";
-        string metafile = asset_temporary + ".meta";
-        if (File.Exists(asset_temporary))
-        {
-
-            File.Move(asset_temporary, asset_new + ".holdable");
-            Debug.Log(metafile);
-        }
-
-        AssetDatabase.Refresh();
-        Debug.ClearDeveloperConsole();
-
-        string whichHand = holdableLHand == "true" ? "left" : "right";
-        EditorUtility.DisplayDialog("Build Success", $"{holdableName} was successfully built for your {whichHand} hand.", "OK");
-    }
-}
+﻿//What 's with these homies, dissing my girl?
+//Why do they gotta front ?
+//What did we ever do to these guys
+//That made them so violent?
+//Woo-hoo
+//But you know I'm yours
+//Woo-hoo
+//And I know you're mine
+//Woo-hoo
+//(And that's for all time)
+//Ooh-wee-hoo, I look just like Buddy Holly
+//Oh-oh, and you're Mary Tyler Moore
+//I don't care what they say about us anyway
+//I don't care about that
+//Don't you ever fear, I'm always near
+//I know that you need help
+//Your tongue is twisted, your eyes are slit
+//You need a guardian
+//Woo-hoo
+//And you know I'm yours
+//Woo-hoo
+//And I know you're mine
+//Woo-hoo
+//(And that's for all time)
+//Ooh-wee-hoo, I look just like Buddy Holly
+//Oh-oh, and you're Mary Tyler Moore
+//I don't care what they say about us anyway
+//I don't care about that
+//I don't care about that
+//Bang, bang, a knock on the door
+//Another big bang and you're down on the floor
+//Oh no! What do we do?
+//Don't look now, but I lost my shoe
+//I can't run, and I can't kick
+//What's the matter babe, are you feeling sick?
+//What's the matter, what's the matter, what's the matter you?
+//What's the matter babe, are you feeling blue?
+//Ooh-wee-hoo, I look just like Buddy Holly
+//Oh-oh, and you're Mary Tyler Moore
+//I don't care what they say about us anyway
+//I don't care about that
+//I don't care about that
+//I don't care about that
+//I don't care about that
